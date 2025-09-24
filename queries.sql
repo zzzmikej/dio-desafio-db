edf143e584f6e7da4837c1ba858740b7b9cd59b5
zzzmikej <michaelhenrique0022@gmail.com>
2025-09-23 23:31:52 -0300

    Add: Criar tabela desafio_dio_Michael e inserir dados iniciais

---
+++ queries.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE desafio_dio_Michael(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+name VARCHAR(250) NOT NULL,
+course VARCHAR(100) NOT NULL
+)
+
+INSERT INTO desafio_dio_Michael (name, course) VALUES
+('Michael Silva', 'Bootcamp AZ-900')
+
+SELECT * FROM desafio_dio_Michael
